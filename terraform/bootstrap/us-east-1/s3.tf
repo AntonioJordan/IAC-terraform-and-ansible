54eafefcfc092dedd1c3a11747483b4904b9a6b8
Toni <tonijordanrodriguez6@gmail.com>
2025-09-04 15:11:01 +0200

    fix tfsec blunder s3 secutiry

--- terraform/bootstrap/us-east-1/s3.tf
+++ terraform/bootstrap/us-east-1/s3.tf
@@ -9,3 +9,22 @@ resource "aws_s3_bucket_versioning" "tf_state_versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "tf_state_public" {
+  bucket                  = aws_s3_bucket.tf_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#tfsec:ignore:aws-s3-encryption-customer-key
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_enc" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
